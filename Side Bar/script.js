d8ef13434cabc790869a640f9dc8de6c2a931318
George <alximik15@gmail.com>
2023-02-21 15:46:51 +0400

    Finished the sidebar

--- Side Bar/script.js
+++ Side Bar/script.js
@@ -1,1 +1,13 @@
-const bar = document.querySelector('.fa-bars')
+const bars = document.querySelector('.fa-bars')
+const sidebar =document.querySelector('.sidebar')
+const closingButton =document.querySelector('.fa-xmark')
+
+bars.addEventListener('click',()=>{
+sidebar.classList.toggle('show-sidebar');
+});
+
+closingButton.addEventListener('click', ()=>{
+    sidebar.classList.remove('show-sidebar')
+});
+
+
