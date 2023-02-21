863e82d2d5b888ff79885d5dd29c2d17f964373a
George <alximik15@gmail.com>
2023-02-21 21:42:27 +0400

    Finished Css and Html and started JS

---
+++ Q&A/script.js
@@ -0,0 +1,8 @@
+const questions =document.querySelectorAll('.question')
+
+questions.forEach((question)=>{
+    //this goes through each article(question) and selects
+    //each of their buttons
+const btn = question.querySelector('.question-btn')
+
+})
