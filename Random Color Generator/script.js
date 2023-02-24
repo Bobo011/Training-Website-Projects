68ea64cc00900afecfa78da3fa734eb9c31f2032
George <alximik15@gmail.com>
2023-02-24 14:02:30 +0400

    Finished the Project

--- Random Color Generator/script.js
+++ Random Color Generator/script.js
@@ -1,2 +1,13 @@
 const containerEl = document.querySelectorAll('.container')
 
+// Loop through each container and set its background color and text content
+containerEl.forEach(container => {
+  let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  
+  // Set the background color of the container
+  container.style.backgroundColor = randomColor;
+
+  // Create a new text node with the color name and append it to the container
+  const colorName = document.createTextNode(randomColor);
+  container.appendChild(colorName);
+});
