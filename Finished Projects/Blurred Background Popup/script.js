cec3e3c04493400591a5aa408fe5355a639499a3
George <alximik15@gmail.com>
2023-02-26 13:10:32 +0400

    Finished and moved the project

---
+++ Finished Projects/Blurred Background Popup/script.js
@@ -0,0 +1,14 @@
+const buttonEl = document.querySelector(".btn");
+const containerEl = document.querySelector(".container");
+const popupContainerEl = document.querySelector(".popup-container");
+const closeIconEl = document.querySelector(".close-icon");
+
+buttonEl.addEventListener("click", () => {
+  containerEl.classList.add("active");
+  popupContainerEl.classList.remove("active");
+});
+
+closeIconEl.addEventListener("click", () => {
+  containerEl.classList.remove("active");
+  popupContainerEl.classList.add("active");
+});
