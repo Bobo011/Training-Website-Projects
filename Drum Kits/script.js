f84401bb0fbb940be246ea769c0df78828b93a62
George <alximik15@gmail.com>
2023-02-26 17:00:50 +0400

    Created a Drum kit Finished Html added files

---
+++ Drum Kits/script.js
@@ -0,0 +1,4 @@
+const crash = document.querySelector('#square-1')
+const kick = document.querySelector('#square-2')
+const snare = document.querySelector('#square-3')
+const tom = document.querySelector('#square-4')
