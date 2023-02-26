89a62e022af0ae28053fdb13d0f2a95056f6c7c8
George <alximik15@gmail.com>
2023-02-26 19:21:34 +0400

    Made the code a bit cleaner

--- Drum Kits/script.js
+++ Drum Kits/script.js
@@ -1,59 +1,41 @@
 //const's for the squares
-const crashSquareEl = document.querySelector('#square-1')
-const kickSquareEl = document.querySelector('#square-2')
-const snareSquareEl = document.querySelector('#square-3')
-const tomSquareEl = document.querySelector('#square-4')
+const crashSquareEl = document.querySelector("#square-1");
+const kickSquareEl = document.querySelector("#square-2");
+const snareSquareEl = document.querySelector("#square-3");
+const tomSquareEl = document.querySelector("#square-4");
 //Audio files
-const crashAudioEl = new Audio('./crash.wav')
-const kickAudioEl = new Audio('./kick.mp3')
-const snareAudioEl = new Audio('./Snare.mp3')
-const tomAudioEl = new Audio('./tom.mp3')
+const crashAudioEl = new Audio("./crash.wav");
+const kickAudioEl = new Audio("./kick.mp3");
+const snareAudioEl = new Audio("./Snare.mp3");
+const tomAudioEl = new Audio("./tom.mp3");
 
-//Crash Sound
-crashSquareEl.addEventListener('click',()=>{
-crashAudioEl.play();
-})
-document.addEventListener('keydown', (event) => {
-    if (event.keyCode === 67 || event.which === 67) {
-      // Key code 67 corresponds to the "C" key
-      crashAudioEl.play();
-    }
-  });
 
-//Kick Sound
-kickSquareEl.addEventListener('click',()=>{
-    kickAudioEl.play();
+squareSound(crashSquareEl,crashAudioEl,67);
+squareSound(kickSquareEl,kickAudioEl,75);
+squareSound(snareSquareEl,snareAudioEl,83);
+squareSound(tomSquareEl,tomAudioEl,84);
 
-})
-document.addEventListener('keydown', (event) => {
-    if (event.keyCode === 75 || event.which === 75) {
-      // Key code 75 corresponds to the "K" key
 
-      kickAudioEl.play();
-    }
-  });
 
-//Snare Sound
-snareSquareEl.addEventListener('click',()=>{
-snareAudioEl.play();
+function squareSound(el,sound,code){
+    el.addEventListener("click", () => {
+        sound.play();
+          el.style.backgroundSize = '150%';
+        });
+        document.addEventListener("keydown", (event) => {
+          if (event.keyCode === code || event.which === code) {
+            // C key :67
+            //K key: 75
+            //S key: 83
+            //T key: 84
+            sound.play();
+            el.style.backgroundSize = '150%';
+          }
+        
+          setTimeout(function() {
+            el.style.backgroundSize = "100%";
+          }, 1000);
+        });
 
-})
-document.addEventListener('keydown', (event) => {
-    if (event.keyCode === 83 || event.which === 83) {
-      // Key code 83 corresponds to the "S" key
-      snareAudioEl.play();
-    }
-  });
-
-//Tom Audio
-tomSquareEl.addEventListener('click',()=>{
-tomAudioEl.play();
-
-})
-document.addEventListener('keydown', (event) => {
-    if (event.keyCode === 84 || event.which === 84) {
-      // Key code 84 corresponds to the "T" key
-      tomAudioEl.play();
-    }
-  });
+}
 
