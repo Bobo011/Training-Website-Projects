51d2df6e03956459b471e40fc7aa89207dfcc8e0
George <alximik15@gmail.com>
2023-03-02 15:44:36 +0400

    Changed the whole thing

--- Anime Pic Generator/script.js
+++ Anime Pic Generator/script.js
@@ -1,62 +1,24 @@
-const buttonEl = document.querySelector('#btn');
-const imageEl = document.querySelector('.image');
-const imageNameEl = document.querySelector('.image-source');
+const buttonEl = document.querySelector("#btn");
+const animeContainerEl = document.querySelector(".anime-container");
+const animeImgEl = document.querySelector(".anime-img");
+const animeNameEl = document.querySelector(".anime-name");
 
-const url = 'https://api.waifu.pics';
-
-async function getAnimePics() {
-  await fetch(`${url}/sfw/neko`).then((response) => {
-    if (response.ok) {
-      response.json().then((data) => {
-        imageEl.style.display = 'block';
-        imageEl.src = data.url;
-        imageNameEl.style.display = 'block';
-        const imageUrlParts = data.url.split('/');
-        const imageName = imageUrlParts[imageUrlParts.length - 1];
-        imageNameEl.textContent = `${imageName}`;
-      });
-    } else {
-        
-      console.error('Failed to retrieve the image ');
-    }
-  }).catch((error) => {
-    console.error(error);
-  });
-}
-
-buttonEl.addEventListener('click', getAnimePics);
-
-
-
-
-
-
-// async function getQuote(){
-
-//     const url = "https://api.quotable.io/random";
-//     let doubleQuote1 = '\u{201F}';
-//     let doubleQuote2 = '\u{201E}';
-//     try {
-//         buttonEl.disabled = true;
-//         quoteEl.innerText = 'Updating...'
-//         buttonEl.innerText = 'Loading...'
-//         authorEl.style.display ='block'
-//         await fetch(url)
-//            .then((data) => data.json())
-//            .then((item) => {
-//             quoteEl.innerText = `${doubleQuote1}${item.content}${doubleQuote2}`;
-//             authorEl.innerText = '-'+ item.author;
-//             buttonEl.disabled = false;
-//             buttonEl.innerText ='Get A Quote'
-//           });
-//     } catch (error) {
-//         titleEl.style.display ='none'
-//         quoteEl.innerText = 'No Internet Access, Please Try Again later';
-//         authorEl.style.display ='none'
-//         console.log(error);
-//     }
-        
-      
-    
-    
-//     }
+buttonEl.addEventListener("click", async function () {
+  try {
+    //when we write above fetching we can initiate actions while the data is being retrieved
+    buttonEl.disabled = true;
+    buttonEl.innerText ='Loading...'
+    animeNameEl.innerText ='Updating...'
+    const url = "https://api.catboys.com/img";
+    const response = await fetch(url);
+    const data = await response.json();
+    //This actions happen after and if the data is fetched and converted to json
+    animeContainerEl.style.display = "block";
+    animeImgEl.src = data.url;
+    animeNameEl.innerText = data.artist;
+    buttonEl.disabled = false;
+    buttonEl.innerText ='Get Anime'
+  } catch (error) {
+    console.log(error);
+  }
+});
