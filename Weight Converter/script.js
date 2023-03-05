48275839823dbed5b23cfb796f0384a8ae878f01
George <alximik15@gmail.com>
2023-03-05 13:46:34 +0400

    Finished doing the script

--- Weight Converter/script.js
+++ Weight Converter/script.js
@@ -1,4 +1,24 @@
 const inputEl = document.querySelector('#pounds');
-const weightInfoEl= document.querySelector('#weight-info');
-const invalidNumberEl = document.querySelector('#invalid-number');
+const weightInfoEl = document.querySelector('#weight-info');
+const invalidNumberEl = document.querySelector('.invalid-number');
 
+inputEl.addEventListener('input', () => {
+  const pounds = inputEl.value;
+
+  if (pounds < 0) {
+    invalidNumberEl.style.display = 'block';
+    weightInfoEl.style.display = 'none';
+  } else {
+    invalidNumberEl.style.display = 'none';
+    const kilograms = (pounds * 0.45359237).toFixed(2);
+    weightInfoEl.textContent = `Your weight in kg is: ${kilograms}`;
+    weightInfoEl.style.display = 'block';
+  }
+});
+
+setTimeout(() => {
+  inputEl.value = '';
+  weightInfoEl.style.display = 'block';
+  weightInfoEl.textContent = `Your weight in kg is:`
+  invalidNumberEl.style.display = 'none';
+}, 10000);
