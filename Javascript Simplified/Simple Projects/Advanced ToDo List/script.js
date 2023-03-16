da8ac163ecb44b927fa46e48c2c7f9c5bf1377b4
George <alximik15@gmail.com>
2023-03-16 11:39:18 +0400

    Started a new project

--- Javascript Simplified/Simple Projects/Advanced ToDo List/script.js
+++ Javascript Simplified/Simple Projects/Advanced ToDo List/script.js
@@ -5,22 +5,34 @@ const templateEl = document.querySelector('#list-item-template')
 const LOCAL_STORAGE_PREFIX ='ADVANCED TO DO LIST-'
 const TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-todos`
 //This will allow us to save todos in an array and javascript
-const todos =loadTodos()
+let todos =loadTodos()
 todos.forEach(renderTodo)
 
 listEl.addEventListener('change',e =>{
     if(!e.target.matches('[data-list-item-checkbox]')){return}
 
-    //Get the todo that is clicked on
-    //toggle the complete property to be equal to the checkbox value
-    //save updated todo
+    
+    const parent = e.target.closest('.list-item')
+    const todoId = parent.dataset.todoId
+    const todo = todos.find(td=>td.id=== todoId)
+    todo.complete = e.target.checked
+    saveTodos()
 })
 
 
+listEl.addEventListener('click',e=>{
+    if(!e.target.matches('[data-button-delete]')){return}
+const parent = e.target.closest('.list-item')
+const todoId = parent.dataset.todoId
+parent.remove()
+
+todos = todos.filter(todo=> todo.id !== todoId)
+
+saveTodos()
+
+})
 
 
-//Add TODOS
-//User will type in todo and click add todo button.
 formEl.addEventListener('click',e=>{
     e.preventDefault()
 
@@ -54,23 +66,19 @@ listItemEl.dataset.todoId = todo.id
 
 const textElement = templateClone.querySelector('[data-list-item-text]')
 textElement.innerText = todo.name
+
+const checkbox = templateClone.querySelector('[data-list-item-checkbox]')
+checkbox.checked =todo.complete
+
 listEl.appendChild(templateClone)
 
 }
 
-//This should then add the todo to the list above.
-
-//Save TODOS
 
 function saveTodos(){
 localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(todos))
 }
 
-
-//Delete TODOS
-//Complete Todos
-
-//Load Todos
 function loadTodos(){
     //gets the storage key and converts to a string
 const todoString = localStorage.getItem(TODOS_STORAGE_KEY)
