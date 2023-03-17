463d626fdc62cda8a503876d296b7a3b1e8586a1
George <alximik15@gmail.com>
2023-03-17 12:21:26 +0400

    updated the script

--- Javascript Simplified/Simple Projects/Expand Collapse/script.js
+++ Javascript Simplified/Simple Projects/Expand Collapse/script.js
@@ -2,3 +2,36 @@ const cardEl =document.querySelector('.card')
 const expandButtonEl = document.querySelectorAll('.expand-button')
 const cardBodyEl =document.querySelectorAll('.card-body')
 
+//Use can press the expand button which will hide or show the text
+//When the button is pressed a show class must be added to cardbodyEl
+
+
+expandButtonEl.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      if (button.innerText === 'Expand') {
+        button.innerText = 'Collapse'
+        cardBodyEl[index].classList.add('show')
+      } else {
+        button.innerText = 'Expand'
+        cardBodyEl[index].classList.remove('show')
+      }
+    })
+  })
+
+
+
+  // document.addEventListener("click", e => {
+  //   if (!e.target.matches(".expand-button")) return
+  
+  //   const card = e.target.closest(".card")
+  //   const cardBody = card.querySelector(".card-body")
+  
+  //   cardBody.classList.toggle("show")
+  //   if (e.target.innerText === "Expand") {
+  //     e.target.innerText = "Collapse"
+  //   } else {
+  //     e.target.innerText = "Expand"
+  //   }
+  //   // e.target.innerText = e.target.innerText === "Expand" ? "Collapse" : "Expand"
+  // })
+  
