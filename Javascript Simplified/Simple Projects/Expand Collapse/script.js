dc8ac207a18013bfbe23fcac08922f14e84098db
George <alximik15@gmail.com>
2023-03-18 10:16:35 +0400

    Made additions

--- Javascript Simplified/Simple Projects/Expand Collapse/script.js
+++ Javascript Simplified/Simple Projects/Expand Collapse/script.js
@@ -1,37 +1,21 @@
-const cardEl =document.querySelector('.card')
-const expandButtonEl = document.querySelectorAll('.expand-button')
-const cardBodyEl =document.querySelectorAll('.card-body')
+const addCardButtonEl = document.querySelector('#add-card');
+const cardTemplateEl = document.querySelector('#card-template');
+const addCardBtnEl = document.querySelector('#add-card')
+const cardEl = document.querySelector('.card')
 
-//Use can press the expand button which will hide or show the text
-//When the button is pressed a show class must be added to cardbodyEl
+addCardBtnEl.addEventListener('click',()=>{
+  const newSection = cardEl.cloneNode(true);
+			document.body.appendChild(newSection);
 
-
-expandButtonEl.forEach((button, index) => {
-    button.addEventListener('click', () => {
-      if (button.innerText === 'Expand') {
-        button.innerText = 'Collapse'
-        cardBodyEl[index].classList.add('show')
-      } else {
-        button.innerText = 'Expand'
-        cardBodyEl[index].classList.remove('show')
-      }
-    })
-  })
+})
 
 
 
-  // document.addEventListener("click", e => {
-  //   if (!e.target.matches(".expand-button")) return
-  
-  //   const card = e.target.closest(".card")
-  //   const cardBody = card.querySelector(".card-body")
-  
-  //   cardBody.classList.toggle("show")
-  //   if (e.target.innerText === "Expand") {
-  //     e.target.innerText = "Collapse"
-  //   } else {
-  //     e.target.innerText = "Expand"
-  //   }
-  //   // e.target.innerText = e.target.innerText === "Expand" ? "Collapse" : "Expand"
-  // })
-  
+// add event listener to the expand/collapse button
+document.addEventListener("click", e => {
+  if (!e.target.matches(".expand-button")) return;
+  const card = e.target.closest(".card");
+  const cardBody = card.querySelector(".card-body");
+  cardBody.classList.toggle("show");
+  e.target.innerText = e.target.innerText === "Expand" ? "Collapse" : "Expand";
+});
