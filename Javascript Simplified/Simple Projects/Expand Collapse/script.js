c1d34ef05dc87d0d9212f6b989da5e38ae3ec42e
George <alximik15@gmail.com>
2023-03-18 12:09:49 +0400

    Trying

--- Javascript Simplified/Simple Projects/Expand Collapse/script.js
+++ Javascript Simplified/Simple Projects/Expand Collapse/script.js
@@ -1,25 +1,22 @@
-const addCardButtonEl = document.querySelector('#add-card');
-const cardTemplateEl = document.querySelector('#card-template');
-const addCardBtnEl = document.querySelector('#add-card')
-const cardEl = document.querySelector('.card')
-const cardTitleEl = document.querySelector('.card-title')
-let titleNum = 5;
-addCardBtnEl.addEventListener('click',()=>{
-  const newSection = cardEl.cloneNode(true);
-  
-   newSection.querySelector('.card-header span').innerText = `Title ${titleNum}`
-   titleNum++;
-			document.body.appendChild(newSection);
+document.addEventListener("DOMContentLoaded", () => {
+  const addCardButtonEl = document.querySelector('#add-card');
+  const cardTemplateEl = document.querySelector('#card-template');
 
-})
+  let titleNum = 5;
 
+  addCardButtonEl.addEventListener('click', () => {
+    const newSection = cardTemplateEl.cloneNode(true);
+    newSection.querySelector('.card-title').innerText = `Title ${titleNum}`;
+    titleNum++;
+    document.body.appendChild(newSection);
+  });
 
-
-// add event listener to the expand/collapse button
-document.addEventListener("click", e => {
-  if (!e.target.matches(".expand-button")) return;
-  const card = e.target.closest(".card");
-  const cardBody = card.querySelector(".card-body");
-  cardBody.classList.toggle("show");
-  e.target.innerText = e.target.innerText === "Expand" ? "Collapse" : "Expand";
+  // add event listener to the expand/collapse button
+  document.addEventListener("click", e => {
+    if (!e.target.matches(".expand-button")) return;
+    const card = e.target.closest(".card");
+    const cardBody = card.querySelector(".card-body");
+    cardBody.classList.toggle("show");
+    e.target.innerText = e.target.innerText === "Expand" ? "Collapse" : "Expand";
+  });
 });
