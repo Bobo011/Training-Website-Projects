f77759124c1dc862a96e93661a899c67e8c8cde2
George <alximik15@gmail.com>
2023-03-24 15:09:27 +0400

    Created a currency formatter

--- Javascript-Simplified/Part=2/Shopping-Cart/store.js
+++ Javascript-Simplified/Part=2/Shopping-Cart/store.js
@@ -1,7 +1,11 @@
+import { enUS } from 'date-fns/locale'
 import items from './items.json'
 
 const storeItemTemplateEl = document.querySelector('#store-item-template')
-const storeItemContainer = document.querySelector('[data-store-container]')
+const storeItemContainerEl = document.querySelector('[data-store-container]')
+
+const IMAGE_URL = 'https://dummyimage.com/420x260'
+
 
 export function setupStore(){
     items.forEach(renderStoreItem)
@@ -14,4 +18,18 @@ const container =storeItem.querySelector('[data-store-item]')
 container.dataset.itemId = item.id 
 const name  = storeItem.querySelector('[data-name]')
 name.innerText = item.name
+
+const category  = storeItem.querySelector('[data-category]')
+category.innerText = item.category
+
+const image  = storeItem.querySelector('[data-image]')
+image.src = `${IMAGE_URL}/${item.imageColor}/${item.imageColor}`
+
+const price  = storeItem.querySelector('[data-price]')
+const formatter = new Intl.NumberFormat(enUS,{style: 'currency', currency:'USD'})
+price.innerText = formatter.format(item.priceCents / 100) 
+
+
+
+storeItemContainerEl.appendChild(storeItem)
 }
