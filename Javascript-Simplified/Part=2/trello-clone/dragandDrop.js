c4e5baaf34893b479cf403205b371040095466ff
George <alximik15@gmail.com>
2023-03-29 20:16:55 +0400

    Made items dragable and a ghost

--- Javascript-Simplified/Part=2/trello-clone/dragandDrop.js
+++ Javascript-Simplified/Part=2/trello-clone/dragandDrop.js
@@ -1,13 +1,68 @@
 import addGlobalEventListener from "../../../util-scripts/addGlobalEventListener.js";
 
-
-export default function setup(){
-    addGlobalEventListener('mousedown','[data-draggable]', e =>{
-    const mouseMoveFunction = ()=>{
-        console.log('move');
-    }
-document.addEventListener('mousemove',mouseMoveFunction)
-
-        })
+export default function setup() {
+    
+  addGlobalEventListener("mousedown", "[data-draggable]", (e) => {
+    const selectedItem = e.target
+    const itemClone = selectedItem.cloneNode(true)
+    const ghost = selectedItem.cloneNode()
+    const offset = setupDragItems(selectedItem,itemClone,ghost, e)
+    setupDragEvents(selectedItem,itemClone,ghost,offset)
+    
+  });
+  
 }
+
+function setupDragItems(selectedItem,itemClone,ghost, e){
+//get the rectangle dimentions
+const originalRect = selectedItem.getBoundingClientRect()
+const offset ={
+    x:e.clientX - originalRect.left,
+    y:e.clientY - originalRect.top
+}
+selectedItem.classList.add('hide')
+//Item stays the same width without shrinking
+itemClone.style.width = `${originalRect.width}px`
+itemClone.classList.add('dragging')
+positionClone(itemClone, e, offset)
+document.body.append(itemClone)
+//GHOST
+ghost.style.height = `${originalRect.height}px`
+ghost.classList.add('ghost')
+ghost.innerHtml = ''
+selectedItem.parentElement.insertBefore(ghost,selectedItem)
+return offset
+
+}
+
+
+function setupDragEvents(selectedItem,itemClone,ghost,offset){
+      //MOUSE MOVE
+      const mouseMoveFunction = (e) => {
+        positionClone(itemClone, e, offset)
+      };
+      document.addEventListener("mousemove", mouseMoveFunction);
+      //MOUSE UP
+      document.addEventListener(
+        "mouseup",
+        () => {
+          document.removeEventListener("mousemove", mouseMoveFunction);
+          stopDrag(selectedItem,itemClone,ghost)
+        },
+        { once: true }
+      );
+}
+
+function positionClone(itemClone,mousePosition,offset){
+itemClone.style.top =`${mousePosition.clientY - offset.y}px`
+itemClone.style.left =`${mousePosition.clientX - offset.x}px`
+}
+
+
+function stopDrag(selectedItem,itemClone,ghost){
+    selectedItem.classList.remove('hide')
+    itemClone.remove()
+    ghost.remove()
+}
+
 
