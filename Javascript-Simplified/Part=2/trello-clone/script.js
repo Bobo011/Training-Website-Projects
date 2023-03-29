01a059a8fbbbaf978b8f4200eb6a218a809c1057
George <alximik15@gmail.com>
2023-03-29 15:50:31 +0400

    Trying things

--- Javascript-Simplified/Part=2/trello-clone/script.js
+++ Javascript-Simplified/Part=2/trello-clone/script.js
@@ -1,23 +1,28 @@
+//JAVASCRIPT CODE
+
 document.addEventListener("DOMContentLoaded", () => {
   const task = document.querySelectorAll(".task");
   const containers = document.querySelectorAll(".tasks");
 
+document.addEventListener('mouseover',e=>{
+    if(e.target.matches('.task')){
+        e.target.draggable=true
+        e.target.classList.add('data-draggable')
+    }
+})
+
   function drag() {
-    document.addEventListener("mouseover", (event) => {
-      if (event.target.matches(".task")) {
-        event.target.setAttribute("draggable", true);
-        
-        document.addEventListener("dragstart", (e) => {
-        
-            e.dataTransfer.setData("text/plain", e.target.id);
-            setTimeout(() => {
-              e.target.classList.add("hide");
-            }, 0);
-          });
-      }
-      
+    task.forEach((task) => {
+      task.addEventListener("dragstart", (e) => {
+        const id = e.target.getAttribute("data-draggable");
+        e.dataTransfer.setData("text/plain", id);
+        setTimeout(() => {
+          e.target.classList.add("hide");
+        }, 0);
+      });
     });
   }
+  
 
   function dropped() {
     containers.forEach((container) => {
@@ -46,10 +51,20 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function drop(e) {
-    e.target.classList.remove('hide')
-    e.target.classList.remove('dragging-over')
+    e.preventDefault();
+    // get the draggable element
+    const id = e.dataTransfer.getData("text/plain");
+    const draggable = document.querySelector(`[data-draggable='${id}']`);
+    // add it to the drop target
+    e.target.querySelector(".tasks").appendChild(draggable);
+    draggable.classList.remove("hide");
+    e.target.classList.remove("hide");
+    e.target.classList.remove("dragging-over");
   }
+  
 
   drag();
   dropped();
 });
+
+//END OF CODE
