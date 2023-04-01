c098b1f19c332b4e9970f41b6a269f1b0acf4474
George <alximik15@gmail.com>
2023-04-01 14:12:01 +0400

    Added Local storage

--- Javascript-Simplified/Part=2/trello-clone/script.js
+++ Javascript-Simplified/Part=2/trello-clone/script.js
@@ -1,10 +1,59 @@
 //JAVASCRIPT CODE
 import setupDragAndDrop from "./dragandDrop.js";
-
+import {v4 as uuidV4} from 'uuid'
+const STORAGE_PREFIX = 'TRELLO_CLONE'
+const LANES_STORAGE_KEY =`${STORAGE_PREFIX}-lanes`
+const DEFAULT_LANES = {
+    backlog:[{id:uuidV4(), text: 'Create your first task'}],
+    doing:[],
+    done:[]
+}
 setupDragAndDrop(onDragComplete)
 
+const lanes = loadLanes()
+renderTasks()
+
 function onDragComplete(e){
-    console.log(e);
+    const startLaneId = e.startZone.dataset.laneId
+    const endLaneId = e.endZone.dataset.laneId
+    const startLaneTasks = lanes[startLaneId]
+    const endLaneTasks = lanes[endLaneId]
+
+    const task = startLaneTasks.find(t => t.id=== e.dragElement.id)
+    startLaneTasks.splice(startLaneTasks.indexOf(task),1)
+    endLaneTasks.splice(e.index, 0, task)
+    saveLanes()
+}
+
+function saveLanes(){
+    localStorage.setItem(LANES_STORAGE_KEY, JSON.stringify(lanes))
+}
+
+function loadLanes() {
+    const laneJson = localStorage.getItem(LANES_STORAGE_KEY)
+    return JSON.parse(laneJson) || DEFAULT_LANES
+}
+
+function renderTasks(){
+    Object.entries(lanes).forEach(obj => {
+        const laneId =obj[0]
+        const tasks = obj[1]
+        const lane  = document.querySelector(`[data-lane-id="${laneId}"]`)
+        tasks.forEach(task=>{
+            const taskElement = createTaskElement(task)
+            lane.append(taskElement)
+        })
+    })
+}
+
+function createTaskElement(task){
+    const element = document.createElement('div')
+    element.id = task.id
+    element.innerText = task.text
+    element.classList.add('task')
+    element.dataset.draggable  = true
+
+    return element
 }
 
 //END OF CODE
