703999d574b441d02ced8b157d0407a3d5061507
George <alximik15@gmail.com>
2023-04-05 15:04:22 +0400

    Added Button elements

--- Javascript-Simplified/Part=2/Destructuring/script1.js
+++ Javascript-Simplified/Part=2/Destructuring/script1.js
@@ -1,3 +1,22 @@
+import addGlobalEventListener from '/util-scripts/addGlobalEventListener.js'
+//Have element the add button
+const addBtn = document.querySelector('.submit')
+
+//When one is pressed, it must be highlited, while others should not
+const allBtn = document.querySelector('.filter-btn.all')
+const notCompletedBtn = document.querySelector('.filter-btn.not-completed')
+const completedBtn = document.querySelector('.filter-btn.completed')
+//the class Active highlights any button
+addGlobalEventListener('click','.filter-btn.all',e=>{
+  console.log('clicked');
+})
+addGlobalEventListener('click','.filter-btn.not-completed',e=>{
+  console.log('clicked');
+})
+addGlobalEventListener('click','.filter-btn.completed',e=>{
+  console.log('clicked');
+})
+
 
 
 
